48ef7b6e849e2f0a89f2347010154cb7e0be0e9d
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-03-25 22:56:35 +0000

    Update clone in series.sh

--- graph/Extension/Template/Kickstart.nvim/Problem:/git clone Timeout/Solution:/clone in series.sh
+++ graph/Extension/Template/Kickstart.nvim/Problem:/git clone Timeout/Solution:/clone in series.sh
@@ -7,6 +7,7 @@ repos=(
 "https://github.com/L3MON4D3/LuaSnip.git",
 "https://github.com/mason-org/mason.nvim.git",
 "https://github.com/nvim-mini/mini.nvim.git",
+"https://github.com/nvim-telescope/telescope-fzf-native.nvim.git",
 "https://github.com/nvim-telescope/telescope.nvim.git",
 "https://github.com/nvim-treesitter/nvim-treesitter.git",
 "https://github.com/neovim/nvim-lspconfig.git",

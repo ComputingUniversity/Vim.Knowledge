689c4a71e87924fb49630629ccdfbaaadfbe7ca8
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-03-25 23:06:35 +0000

    Update clone in series.sh

--- graph/Extension/Template/Kickstart.nvim/Problem:/git clone Timeout/Solution:/clone in series.sh
+++ graph/Extension/Template/Kickstart.nvim/Problem:/git clone Timeout/Solution:/clone in series.sh
@@ -10,15 +10,18 @@ repos=(
 "https://github.com/NMAC427/guess-indent.nvim.git",
 "https://github.com/L3MON4D3/LuaSnip.git",
 "https://github.com/mason-org/mason-lspconfig.nvim.git",
-
-"https://github.com/folke/tokyonight.nvim.git",
 "https://github.com/mason-org/mason.nvim.git",
-"https://github.com/nvim-lua/plenary.nvim.git",
+"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim.git",
 "https://github.com/nvim-mini/mini.nvim.git",
+"https://github.com/neovim/nvim-lspconfig.git",
+"https://github.com/nvim-treesitter/nvim-treesitter.git",
+"https://github.com/nvim-lua/plenary.nvim.git",
 "https://github.com/nvim-telescope/telescope-fzf-native.nvim.git",
 "https://github.com/nvim-telescope/telescope.nvim.git",
-"https://github.com/nvim-treesitter/nvim-treesitter.git",
-"https://github.com/neovim/nvim-lspconfig.git",
+"https://github.com/nvim-telescope/telescope-ui-select.nvim.git",
+"https://github.com/folke/todo-comments.nvim.git",
+"https://github.com/folke/which-key.nvim.git",
+"https://github.com/folke/tokyonight.nvim.git",
 )
 
 for r in repos
